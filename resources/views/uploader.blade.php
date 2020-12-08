71c786ed156246f8b6498e6ec3a935195a4f642b
DESKTOP-CI6NLFB\admin <tultutu4@gmail.com>
2020-12-08 20:34:10 +0900

    no message

--- resources/views/uploader.blade.php
+++ resources/views/uploader.blade.php
@@ -5,18 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>uploader</title>
+  <style>
+    .global-container{
+      width: 402px;
+      height: 469px;
+      margin: 0 auto;
+
+      background: #FFFFFF;
+      box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+      border-radius: 12px;
+    }
+    .test{
+      color: red;
+    }
+  </style>
 </head>
 <body>
   <div class="global-container">
 
-    <h1>Upload your image</h1>
+    <h1 class="page-title">Upload your image</h1>
     <p>File should be Jpeg, Png,...</p>
 
     <div class="upload-space"> 
       <p>Drag & Drop your image here</p>
     </div>
 
-    <p>Or</p>
+    <p class="test">Or</p>
 
     <button type="button">Choose a file</button>
   </div>
